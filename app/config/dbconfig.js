79ed3d7f0fc610b7c348867d12a9a37ea2967209
TheERW85 <39521333+TheERW85@users.noreply.github.com>
2019-03-18 10:25:42 +0100

    dbconfig

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -11,65 +11,85 @@ let db = new sqlite3.Database('./sqlite.db');
 /* Init car and driver tables if they don't exist */
 let init = function () {
     db.run("CREATE TABLE if not exists installations (" +
-        "NumInstallation INTEGER PRIMARY KEY AUTOINCREMENT," +
-        "NomInstallation TEXT" +
+        "NumInstallation INTEGER PRIMARY KEY," +
+        "NomInstallation TEXT," +
         "CodeINSEE INT," +
         "CodeDepartement INT," +
         "CodePostal INT," +
-        "NomDepartement TEXT" +
-        "NomCommune TEXT" +
-        "Adresse TEXT" +
-        "LocX DECIMAL(9,6)" +
-        "LocY DECIMAL(9,6)" +
-        "DesserteBus BOOLEAN"+
-        "DesserteTrain BOOLEAN"+
-        "DesserteTram BOOLEAN"+
-        "InstalParticuliere  TEXT" +
-        "AccessibleHandicapés  BOOLEAN" +
-        "NbplaceParking  INT" +
-        "NbplaceParkingHandicapés  INT" +
+        "NomDepartement TEXT," +
+        "NomCommune TEXT," +
+        "Adresse TEXT," +
+        "LocX DECIMAL(9,6)," +
+        "LocY DECIMAL(9,6)," +
+        "DesserteBus BOOLEAN,"+
+        "DesserteTrain BOOLEAN,"+
+        "DesserteTram BOOLEAN,"+
+        "InstalParticuliere  TEXT," +
+        "AccessibleHandicapés  BOOLEAN," +
+        "NbplaceParking  INT," +
+        "NbplaceParkingHandicapés  INT," +
         ")");
 
     db.run("CREATE TABLE if not exists equipement (" +
-        "Numequipement INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "Numequipement INTEGER PRIMARY KEY," +
         "Equipement TEXT," +
         "NumInstallation TEXT," +
-        "Typeequipement TEXT" +
-        "Proprietaire TEXT" +
-        "Gestionnaire TEXT" +
-        "Eclairage BOOLEAN" +
-        "Sallepolyvalente BOOLEAN" +
-        "EtabPleinAir BOOLEAN" +
-        "EtabSportifCouvert BOOLEAN" +
-        "NbplaceTribune INT" +
-        "Typedusol TEXT"+
-        "AireEvolLongueur INT" +
-        "AireEvolLargeur INT" +
-        "NbCouloir INT" +
-        "NbVerstiaireStortif INT" +
-        "SonoFixe BOOLEAN" +
-        "TableauFixe BOOLEAN" +
-        "Chronometrage BOOLEAN" +
-        "SanitairePublic BOOLEAN" + //
-        "AcHandMobiAireEvol BOOLEAN" +
-        "AcHandMobiTribune BOOLEAN" +
-        "AcHandMobiVestiaire BOOLEAN" +
-        "AcHandMobiSanitairePublic BOOLEAN" +
-        "AcHandMobiSanitaireSportif BOOLEAN" +
-        "AccueilClub BOOLEAN" +
-        "AccueilSalledeReunion BOOLEAN" +
-        "AccueilBuvette BOOLEAN" +
-        "AccueilInfirmerie BOOLEAN" +
-        "AccueilReception BOOLEAN" +
-        "AccueilLocalRangement BOOLEAN" +
-        "NbcouloirE scalade" +
+        "Typeequipement TEXT," +
+        "Proprietaire TEXT," +
+        "Gestionnaire TEXT," +
+        "Eclairage BOOLEAN," +
+        "Sallepolyvalente BOOLEAN," +
+        "EtabPleinAir BOOLEAN," +
+        "EtabSportifCouvert BOOLEAN," +
+        "NbplaceTribune INT," +
+        "Typedusol TEXT,"+
+        "AireEvolLongueur INT," +
+        "AireEvolLargeur INT," +
+        "NbCouloir INT," +
+        "NbVerstiaireStortif INT," +
+        "SonoFixe BOOLEAN," +
+        "TableauFixe BOOLEAN," +
+        "Chronometrage BOOLEAN," +
+        "SanitairePublic BOOLEAN," + //
+        "AcHandMobiAireEvol BOOLEAN," +
+        "AcHandMobiTribune BOOLEAN," +
+        "AcHandMobiVestiaire BOOLEAN," +
+        "AcHandMobiSanitairePublic BOOLEAN," +
+        "AcHandMobiSanitaireSportif BOOLEAN," +
+        "AccueilClub BOOLEAN," +
+        "AccueilSalledeReunion BOOLEAN," +
+        "AccueilBuvette BOOLEAN," +
+        "AccueilInfirmerie BOOLEAN," +
+        "AccueilReception BOOLEAN," +
+        "AccueilLocalRangement BOOLEAN," +
+        "NbcouloirEscalade INT," +
+        "Hauteurescalade INT,"+
+        "Surfaceescalade INT,"+
+        "Nbairesdesaut INT,"+
+        "Nbairesauthauteur INT,"+
+        "Nbairessautlongueur INT,"+
+        "Nbairessautlongueurtriplesaut INT,"+
+        "Nbairessautlongueurettriplesaut INT,"+
+        "Nbairessautsautoirperche INT,"+
+        "Nbaireslancer INT,"+
+        "Nbairespoid,INT,"+
+        "Nbairesdisque INT,"+
+        "Nbairesjavelot INT,"+
+        "Nombreairesmarteau INT,"+
+        "Nombreaireslancermixtedisquemarteau INT,"+
+        "Longueurbassin DECIMAL(9,6),"+
+        "Largeurbassin DECIMAL(9,6),"+
+        "Profondeurmini DECIMAL(9,6),"+
+        "Profondeurmaxi DECIMAL(9,6),"+
+        "Nbtotaltremplins INT"+
         ")");
 
     db.run("CREATE TABLE if not exists activites (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " firstName TEXT," +
-        " lastName TEXT," +
-        " car INT" +
+        "Activitecode INTEGER " +
+        "Activitelibelle TEXT," +
+        "Numerodelaficheequipement INTEGER," +
+        "Niveaudelactivite TEXT," +
+        "PRIMARY KEY (Numerodelaficheequipement,Activitecode)"+
         ")");
 };
 
