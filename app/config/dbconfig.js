9166557f1ad858f5c4faf4334483fa0912510b2f
TheERW85 <39521333+TheERW85@users.noreply.github.com>
2019-03-18 09:41:31 +0100

    com

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -10,21 +10,75 @@ let db = new sqlite3.Database('./sqlite.db');
 
 /* Init car and driver tables if they don't exist */
 let init = function () {
-    db.run("CREATE TABLE if not exists car (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " maker TEXT," +
-        " model TEXT," +
-        " year INT," +
-        " driver INT" +
+    db.run("CREATE TABLE if not exists installations (" +
+        "NumInstallation INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "NomInstallation TEXT" +
+        "CodeINSEE INT," +
+        "CodeDepartement INT," +
+        "CodePostal INT," +
+        "NomDepartement TEXT" +
+        "NomCommune TEXT" +
+        "Adresse TEXT" +
+        "LocX  DECIMAL(9,6)" +
+        "LocY DECIMAL(9,6)" +
+        "DesserteBus BOOLEAN"+
+        "DesserteTrain BOOLEAN"+
+        "DesserteTram BOOLEAN"+
+        "InstalParticuliere  TEXT" +
+        "AccessibleHandicapés  BOOLEAN" +
+        "NbplaceParking  INT" +
+        "NbplaceParkingHandicapés  INT" +
         ")");
 
-    db.run("CREATE TABLE if not exists driver (" +
+    db.run("CREATE TABLE if not exists equipement (" +
+        "Numequipement INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "equipement TEXT," +
+        "NumInstallation TEXT," +
+        "Typeequipement TEXT" +
+        "Proprietaire TEXT" +
+        "Gestionnaire TEXT" +
+        "Eclairage BOOLEAN" +
+        "Sallepolyvalente BOOLEAN" +
+        "EtabPleinAir BOOLEAN" +
+        "EtabSportifCouvert BOOLEAN" +
+        "NbplaceTribune INT" +
+        "typedusol TEXT"+
+        "AireEvolLongueur INT" +
+        "AireEvolLargeur INT" +
+        "NbCouloir INT" +
+        "NbVerstiaireStortif INT" +
+        "SonoFixe BOOLEAN" +
+        "TableauFixe BOOLEAN" +
+        "Chronometrage BOOLEAN" +
+        "SanitairePublic BOOLEAN" +
+        "AcHandMobiAireEvol BOOLEAN" +
+        "AcHandMobiTribune BOOLEAN" +
+        "AcHandMobiVestiaire BOOLEAN" +
+        "AcHandMobiSanitairePublic BOOLEAN" +
+        "AcHandMobiSanitaireSportif BOOLEAN" +
+        "AcHandSensoAireEvol BOOLEAN" +
+        "AcHandSensoTribune BOOLEAN" +
+        "AcHandSensoVestiaire BOOLEAN" +
+        "AcHandSensoSanitairePublic BOOLEAN" +
+        "AcHandSensoSanitaireSportif BOOLEAN" +
+        "AccueilClub BOOLEAN" +
+        "AccueilSalledeReunion BOOLEAN" +
+        "AccueilBuvette BOOLEAN" +
+        "AccueilInfirmerie BOOLEAN" +
+        "AccueilReception BOOLEAN" +
+        "AccueilLocalRangement BOOLEAN" +
+        "NbcouloirE scalade" +
+        ")");
+
+    db.run("CREATE TABLE if not exists activites (" +
         "id INTEGER PRIMARY KEY AUTOINCREMENT," +
         " firstName TEXT," +
         " lastName TEXT," +
         " car INT" +
         ")");
 };
+
+
 
 module.exports = {
     init: init,
