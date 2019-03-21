95bad1188886401bddb64fec12da6adcbc772953
TheERW85 <39521333+TheERW85@users.noreply.github.com>
2019-03-21 16:52:56 +0100

    test requete fonctionne

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const bodyParser = require("body-parser");
 /* Database configuration */
 const database =                                                                                                                                                                                                                                                                                                                                                                                                                                                        require('./app/config/dbconfig');
 
-/* Init database */
-database.init();
+/** Init database **/
 
-/* Init server listening */
+//database.init();
+
+/** Init server listening **/
 const port = process.argv[2] || 3000;
 app.listen(port, function () {
     console.log("Server listening on port : " + port);
 });
 
-/* Express configuration */
+/** Express configuration **/
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 /* Router configuration */
 const REST_API_ROOT = '/api';
 app.use(REST_API_ROOT, require('./app/routes/router'));
+
+
+/** test **/
+
+database.db.all("select * from activites ", function (err, rows) {
+
+    rows.forEach(function (element) {
+        console.log(element);
+    })
+});
